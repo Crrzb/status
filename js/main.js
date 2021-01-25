ee1b45d667825d4d2250288faa287e93cc7c40b9
Crrzb <41073423+Crrzb@users.noreply.github.com>
2021-01-24 22:06:35 -0600

    Actalice vacunas 24enero

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 let totalVacunadaMundo =57287507;
-let totalVacunadaMexico = 552335;
+let totalVacunadaMexico = 629626;
 
 
 
@@ -7,7 +7,7 @@ let totalVacunadaMexico = 552335;
 let poblacionMundo =  7874965731;
 let poblacionMexico = 130262220;
 
-const fecha ="21-Enero-2021";
+const fecha ="24-Enero-2021";
 const hora = "19:23";
 
 
